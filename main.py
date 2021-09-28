57912f36ac336c3ffc6d750be3a8182465ba2aa7
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-09-28 09:45:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Band Name Generator - areview for printing, string manipulation and variable declaring in python
+print("Welcome to the Band Name Generator")
+
+city = input("Which City did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+
+band_name = city + " " + pet
+print("Your Band Name is: \n" + band_name)
