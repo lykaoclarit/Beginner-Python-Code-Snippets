9663774d0d2fa8de44be7999c9a6d91017210ff9
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-09-29 09:33:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Day 1 of 100 - Python Challenge
-#Band Name Generator - areview for printing, string manipulation and variable declaring in python
+#Band Name Generator -  printing, string manipulation and variable declaring in python
 print("Welcome to the Band Name Generator")
 
 city = input("Which City did you grew up in?\n")
@@ -7,3 +7,11 @@ pet = input("What is the name of your pet?\n")
 
 band_name = city + " " + pet
 print("Your Band Name is: \n" + band_name)
+
+#Day 2 0f 100
+#Learning about Data Types, subscripting, conversion of data types
+#Mathematical Operations in Python
+
+two_digit_number = input("Type a two digit number: ")
+output = (int(two_digit_number[0]) + int(two_digit_number[1])) 
+print(output)
