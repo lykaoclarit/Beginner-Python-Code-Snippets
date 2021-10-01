2a5f0ba9e93d5a480cf7db86548d3bf880782a35
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-10-01 13:14:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -99,3 +99,48 @@ if year % 4  == 0:
 else:
   print("Not a leap year")
 
+#Pizza Order
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+add_cheese = input("Do you want extra cheese? Y or N ")
+
+bill = 0 
+
+if size == "S":
+  bill += 15
+  if add_pepperoni == "Y":
+    bill +=2
+    if add_cheese == "Y":
+      bill +=1
+      print(f"Your total bill is ${bill}")
+    else:
+      print(f"Your total bill is ${bill}")
+  else:
+    print(f"Your total bill is ${bill}")
+elif size == "M":
+  bill += 20
+  if add_pepperoni == "Y":
+    bill +=3
+    if add_cheese == "Y":
+      bill +=1
+      print(f"Your total bill is ${bill}")
+    else:
+      print(f"Your total bill is ${bill}")
+  else:
+    print(f"Your total bill is ${bill}")
+elif size == "L":
+  bill += 25
+  if add_pepperoni == "Y":
+    bill +=3
+    if add_cheese == "Y":
+      bill +=1
+      print(f"Your total bill is ${bill}")
+    else:
+      print(f"Your total bill is ${bill}")
+  else:
+    print(f"Your total bill is ${bill}")
+else:
+  print(f"Your total bill is {bill}")
+
+
