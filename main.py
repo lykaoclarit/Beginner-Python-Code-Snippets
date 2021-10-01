0bc513f56450773dfcab454d90e71d48dc41c751
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-10-01 14:24:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -141,4 +141,34 @@ elif size == "L":
   else:
     print(f"Your total bill is ${bill}")
 
+#Love Calculator
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+combine_name = name1 + name2
+
+combine_name.lower()
+t = combine_name.count("t")
+r = combine_name.count("r")
+u = combine_name.count("u")
+e = combine_name.count("e")
+
+l = combine_name.count("l")
+o = combine_name.count("o")
+v = combine_name.count("v")
+e = combine_name.count("e")
+
+true = int(t) + int(r) + int(u) + int(e) 
+love = int(l) + int(o) + int(v) + int(e)
+
+score = int(str(true) + str(love))
+
+
+if (score < 10) or (score > 90):
+  print(f"Your score is {score}%, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+  print(f"Your score is {score}%, you are alright together.")
+else:
+  print(f"Your score is {score}%.")
 
