ec80db67a8aeb98f204f04ec8404148c39998fe8
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-10-12 01:25:11 +0800

    Update snippets1.py

--- snippets1.py
+++ snippets1.py
@@ -370,3 +370,17 @@ for score in student_scores:
     highest_score = score
 
 print(f"The highest score in the class is: {highest_score}")
+
+#Range func
+
+total = 0
+for n in range(2, 101, 2):
+  total += n
+print(total)
+
+#Other sol
+total2 = 0
+for n in range (1, 101):
+  if n % 2 == 0:
+    total2 += n
+print(total2)
