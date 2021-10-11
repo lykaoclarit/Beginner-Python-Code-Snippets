b348dd69fd02f0aa82d54b5e38054d38a46cc45d
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-10-12 01:06:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -356,3 +356,17 @@ for student in student_heights:
 
 average_height = round(total_height / number_of_students)
 print(average_height)
+
+#Getting max value with for loop
+student_scores = input("Input a list of student scores:\n").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
