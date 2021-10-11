a0ffae809a7176428c9cc41838cdc1e90939b916
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-10-12 00:47:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -330,3 +330,11 @@ else:
   print("Computer\n")
   print(rps[computer]) 
   print("You lose!")
+
+#Day 5 - Loops
+#for loop sample
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+  print(fruit)
+  print(fruit + " Pie")
