be671d4a1cdae4d0b2106100f400d2b9c6d1e9aa
Lyka <66162537+lykaoclarit@users.noreply.github.com>
2021-10-12 01:35:44 +0800

    Update snippets1.py

--- snippets1.py
+++ snippets1.py
@@ -384,3 +384,14 @@ for n in range (1, 101):
   if n % 2 == 0:
     total2 += n
 print(total2)
+
+#FizzBuzz
+for n in range(1, 101):
+  if n % 3 == 0 and n % 5 == 0:
+    print("FizzBuzz!")
+  elif n % 3 == 0:
+    print("Fizz!")
+  elif n % 5 == 0:
+    print("Buzz!")
+  else:
+    print(n)
